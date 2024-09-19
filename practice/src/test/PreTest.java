f90bf5e23c3168b0dce9c75ca9c1b9f1b2acd525
yongh <yongh@kwonyonghwan>
2024-09-20 00:22:12 +0900

    commit test

--- practice/src/test/PreTest.java
+++ practice/src/test/PreTest.java
@@ -1,37 +1,30 @@
-package test;
-
-import java.util.Scanner;
-
-public class PreTest {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		 int inputNumber = sc.nextInt();
-//		int inputNumber = 99999;
-		int judgeNumber = 9;
-		StringBuilder result = new StringBuilder(inputNumber);
-		char[] broadArray = { ' ', 'ÀÏ', 'ÀÌ', '»ï', '»ç', '¿À', 'À°', 'Ä¥', 'ÆÈ', '±¸' };
-		char[] pointArray = { ' ', '½Ê', '¹é', 'Ãµ', '¸¸' };
-		int pointIndex = 0;
-
-		while (inputNumber > (judgeNumber / 10)) {
-
-			int stepNumber = inputNumber % (judgeNumber + 1) / ((judgeNumber + 1) / 10);
-
-			if (stepNumber > 0) {
-				result.append(pointArray[pointIndex]);
-				if (pointIndex < 1 || stepNumber > 1) {
-					result.append(broadArray[stepNumber]);
-				}
-			} // ifNotZero
-
-			judgeNumber = judgeNumber * 10 + 9;
-			pointIndex++;
-		} // whileStep
-
-		System.out.println(result.reverse());
-	}// main
-
-}// class
+/*
+ * package test;
+ * 
+ * import java.util.Scanner;
+ * 
+ * public class PreTest {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * Scanner sc = new Scanner(System.in);
+ * 
+ * int inputNumber = sc.nextInt(); // int inputNumber = 99999; int judgeNumber =
+ * 9; StringBuilder result = new StringBuilder(inputNumber); // char[]
+ * broadArray = { ' ', 'ï¿½ï¿½', 'ï¿½ï¿½', 'ï¿½ï¿½', 'ï¿½ï¿½', 'ï¿½ï¿½', 'ï¿½ï¿½', 'Ä¥', 'ï¿½ï¿½', 'ï¿½ï¿½' }; //
+ * char[] pointArray = { ' ', 'ï¿½ï¿½', 'ï¿½ï¿½', 'Ãµ', 'ï¿½ï¿½' }; int pointIndex = 0;
+ * 
+ * while (inputNumber > (judgeNumber / 10)) {
+ * 
+ * int stepNumber = inputNumber % (judgeNumber + 1) / ((judgeNumber + 1) / 10);
+ * 
+ * if (stepNumber > 0) { result.append(pointArray[pointIndex]); if (pointIndex <
+ * 1 || stepNumber > 1) { result.append(broadArray[stepNumber]); } } //
+ * ifNotZero
+ * 
+ * judgeNumber = judgeNumber * 10 + 9; pointIndex++; } // whileStep
+ * 
+ * System.out.println(result.reverse()); }// main
+ * 
+ * }// class
+ */
